71646ea0e65afff63b3fae7bbccfe490699054c8
Farhan MS <farhanmohaemin14@gmail.com>
2024-10-19 19:33:14 +0700

    revisi model api top anime

--- lib/api/api_top_anime.dart
+++ lib/api/api_top_anime.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Map<String, List<Anime>>> getTopAnime({
+Future<Iterable<Anime>> getTopAnime({
   required String type,
   required String filter,
   required int page,
@@ -15,10 +15,8 @@ Future<Map<String, List<Anime>>> getTopAnime({
   final response = await http.get(Uri.parse(link.linkListTopAnime(type, filter, page, limit)));
    if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
-    List<Anime> anime = (data['data'] as List).map((animeJson) => Anime.fromJson(animeJson)).toList();
-    return {
-      'data' : anime,
-    };
+    final List<dynamic> animeList = data['data'];
+    return animeList.map((animeJson) => Anime.fromJson(animeJson));
    }else{
     debugPrint("Error: ${response.statusCode}");
     debugPrint("Body: ${response.body}");
